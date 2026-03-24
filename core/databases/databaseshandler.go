02483100de13b74f8dcc508653c3a9342eea6407
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-03-24 17:58:07 -0300

    feat: database handler and dw setuper

--- core/databases/databaseshandler.go
+++ core/databases/databaseshandler.go
@@ -2,14 +2,17 @@ package databases
 
 import (
 	"errors"
-	"path/filepath"
+	"path"
 
+	"github.com/DotNicolasPenha/Metrics-Tracker/core/datawork"
 	"github.com/google/uuid"
 )
 
 type DataBaseHandler struct {
-	databaseJsonPath string
 	Databases        []DataBase
+	databasedirpath  string
+	databasejsonname string
+	dwjson           datawork.DataWorkHandler
 }
 
 type DataBase struct {
@@ -18,35 +21,47 @@ type DataBase struct {
 	Name     string         `json:"name"`
 	Url      string         `json:"url"`
 	Limits   DataBaseLimits `json:"limits"`
+	Blocks   Blocks         `json:"blocks"`
 	Driverid uuid.UUID      `json:"driverid"`
+	Proxyid  uuid.UUID      `json:"proxyid"`
+}
+type Blocks struct {
+	Queries []string           `json:"queries"`
+	Clients ClientsBlockLimits `json:"clients"`
+}
+type ClientsBlockLimits struct {
+	MaxQueriesPerDay int64 `json:"mqpDay"`
+	MaxQueriesPerMin int64 `json:"mpqMin"`
 }
 
+// block de database when break the limit
 type DataBaseLimits struct {
-	Maxconn        int64 `json:"maxconn"`
-	Maxqueriesconn int64 `json:"maxqueriesconn"`
-	MaxcpuPerconn  int64 `json:"maxcpuperconn"`
-	MaxramPerconn  int64 `json:"maxramperconn"`
-	MaxdiskPerconn int64 `json:"maxdiskperconn"`
-	Maxram         int64 `json:"maxram"`
-	Maxcpu         int64 `json:"maxcpu"`
-	Maxdisk        int64 `json:"maxdisk"`
+	Maxconn int64 `json:"maxconn"`
+	MaxQPS  int64 `json:"maxqps"`
 }
 
-func NewDataBaseHandler(metrackerdirpath string) (*DataBaseHandler, error) {
-	if metrackerdirpath == "" {
-		return nil, errors.New("metrackerdirpath param is empty")
+func NewDataBaseHandler(mtdbpath string, namejsonfile string) (*DataBaseHandler, error) {
+	if mtdbpath == "" {
+		return nil, errors.New("mtdbpath param is empty")
 	}
-
-	path := filepath.Join(metrackerdirpath, "databases.json")
-
+	if namejsonfile == "" {
+		return nil, errors.New("namejsonfile param is empty")
+	}
 	dbh := &DataBaseHandler{
-		databaseJsonPath: path,
+		databasedirpath:  mtdbpath,
 		Databases:        []DataBase{},
+		databasejsonname: namejsonfile,
 	}
-
-	if err := dbh.load(); err != nil {
+	if err := dbh.dataWorkJsonSetup(); err != nil {
 		return nil, err
 	}
-
 	return dbh, nil
+}
+func (dbh *DataBaseHandler) dataWorkJsonSetup() error {
+	dwh, err := datawork.NewDataWorkHandler(dbh.databasedirpath)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+	dbh.dwjson = *dwh
+	return dwh.Load([]DataBase{}, path.Join(dbh.databasedirpath, dbh.databasejsonname))
 }
