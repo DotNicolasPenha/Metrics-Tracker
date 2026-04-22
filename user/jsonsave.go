20c154165a53ce2588360a0e6dbbb160d69f1b6e
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-04-22 15:56:26 -0300

    fix: user package name

--- user/jsonsave.go
+++ user/jsonsave.go
@@ -1,27 +1,9 @@
-package configurations
+package user
 
 import (
 	"encoding/json"
 	"os"
-
-	"github.com/DotNicolasPenha/Metrics-Tracker/interceptor"
 )
-
-type User struct {
-	Configurations Configurations            `json:"configurations"`
-	Interceptors   []interceptor.Interceptor `json:"interceptors"`
-}
-type Configurations struct {
-	Limits       Limits        `json:"limits"`
-	BlockQueries []BlockQuerie `json:"block_queries"`
-}
-type BlockQuerie struct {
-	Query  string `json:"query"`
-	Retrys int    `json:"retrys"`
-}
-type Limits struct {
-	MaxActConnections int `json:"max_active_connections"`
-}
 
 func SaveUser(user User, path string) error {
 	jsonData, err := json.MarshalIndent(user, "", "  ")
